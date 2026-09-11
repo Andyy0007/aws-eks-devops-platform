cddc0da33fe1735dc60f7b02b34f418365224063
anadi mishra <98818583+Andyy0007@users.noreply.github.com>
2026-09-12 04:14:20 +0530

    infra: provision AWS VPC and EKS

---
+++ terraform/main.tf
@@ -0,0 +1,51 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 6.0"
+
+  name = "${var.cluster_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  private_subnets = ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24"]
+  public_subnets  = ["10.20.101.0/24", "10.20.102.0/24", "10.20.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = var.cluster_name
+  kubernetes_version = "1.33"
+
+  endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    general = {
+      instance_types = var.instance_types
+      min_size       = 2
+      max_size       = 4
+      desired_size   = 2
+    }
+  }
+
+  tags = {
+    Project = "devops-production-project"
+    Owner   = "Anadi"
+  }
+}
